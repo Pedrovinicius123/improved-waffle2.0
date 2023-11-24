d5566e361684e1474c00f37293cca3a9c3aa4bf3
Pedro Vinícius de Almeida Silva Xavier <120400955+Pedrovinicius123@users.noreply.github.com>
2023-11-24 19:23:23 +0000

    Help

--- test1.py
+++ test1.py
@@ -2,16 +2,8 @@ import math
 
 from translator.conversor import find_value, get_decompacted_value
 
-upper, ex_value, processes, mean, result, length= find_value(365)
+upper, ex_value, inc, mean, result, length = find_value(60)
 
-
-
-decompacted = get_decompacted_value(upper, processes=processes, alpha=mean, expo=length, val=ex_value)
-
-print(upper)
-
-print(mean, processes)
-
-print(result, processes)
-
+decompacted = get_decompacted_value(upper, ex_value, inc, mean, length)
+print(result)
 print(decompacted)
--- translator/conversor.py
+++ translator/conversor.py
@@ -1,4 +1,5 @@
 # With the help of 'Acervo Lima' and 'DelftStack' docs ;)
+import decimal
 from math import log2, ceil, log, floor
 from time import sleep
 import numpy as np
@@ -20,23 +21,38 @@ def find_value(value):
         length = len(bin(value)[2:])
         power = 2 ** length
 
-        value = power-value   
+        value = power-value
 
-        powers.append(length)     
+        powers.append(length) 
 
-        if log2(value) - round(log2(value), 1) == 0 or nums.count(value) > 1:
-
-            break
+        ex_value = value 
 
         nums.append(value)
 
-        ex_value = value
+
+        if log2(value)%2 == 0:
+            
+            print(nums)
+
+            return nums[0], ex_value, inc, power, log2(abs(value-2**length)), length
+
+        
+
+        elif log2(value) - round(log2(value), 1) == 0 or nums.count(value) > 1:
+
+            break
+
+       
+
+        print(nums)
         
                   
         inc += 1
 
     for i in range(length):
         val = 2**i
+
+        print(val)
         
         result = 2**length-val
 
@@ -46,106 +62,26 @@ def find_value(value):
 
             return nums[len(nums)-2], ex_value, inc, return_mean_variation(powers), log2(abs(result-2**length)), length
 
-def get_decompacted_value(upper, expo, val, alpha, processes):
+def get_decompacted_value(nums, ex_value, inc, mean, length):
 
-    counter = 0
-    static_expo = upper
+    #Developement of the core of the decompact function
 
-    print('static_expo, ',static_expo)
+    import numpy as np
 
-    print('pre-expo: ', expo)
-
-    expo = floor(log2(expo+2**expo))
-
-    print('post-expo: ', expo)
-
-    print(static_expo)
-
-    if processes == 1:
-        return 2**floor(expo+alpha) - upper
-
-
-    zeros_array = np.array([])
     ones_array = np.array([])
+    zeros_array = np.array([])
     total_array = np.array([])
-    regression_array = np.array([])    
+
+    if inc == 0:
+        return mean - nums
+
+    for pre_processing in range(inc):
+        
+        
+
+         ones_array = np.append(ones_array, )
 
     
-    for index in range(processes):
-
-        validation = expo + floor(alpha*index)
-
-        if index == 0:
-            pass
-
-        else:
-
-            zeros_array = np.append(zeros_array, floor(validation))
-            ones_array = np.append(ones_array, floor(validation+1))
-
-    total_array = np.append(total_array, np.concatenate((zeros_array, ones_array)))
-    num, counts = np.unique(np.sort(total_array), return_counts=True)
-
-    result_array = np.column_stack((num, counts))
-
-    print(result_array, ' Result array')
-
-    for pre_regression in range(processes):
-
-        value = result_array[pre_regression]
-
-        if value[1] > 1:
-            counter += 1
-
-            if counter >= 2:
-                pass
-
-            elif counter == 1:
-                major = value
-
-            else:
-                major = value if value > major else major
-
-
-        else:
-            regression_array = np.append(regression_array, value[0])
-
-    counter = 0
-
-    print('Pre regression arr: ', regression_array)
-
-    for value in regression_array:
-
-        regression_array[counter] = floor(expo*alpha) + value
-        counter += 1
-
-    print('Regression arr: ',regression_array)
-
-    for regression in range(processes):
-
-        try:
-
-            print(regression)
-        
-            print('Exponential, ',expo)
-            
-            value = 2**regression_array[regression]
-            
-            static_expo = value - static_expo
-            val = static_expo
-
-            print(val)  
-
-            sleep(1)
-
-
-        except Exception as err:
-            print('Array index out of bounds, continue process...')
-            break
-
-     
-
-    return val
 
 def return_mean_variation(args:list):
 
@@ -166,7 +102,15 @@ def return_mean_variation(args:list):
 
             prox.append(inc)
 
-    variation = sum(prox)/len(prox)
+    print('Prox: ',prox)
 
-    return variation
+    try:
+
+        variation = sum(prox)/len(prox)
+
+    except Exception:
+        print(':D')
+
+    else:
+        return variation
     
